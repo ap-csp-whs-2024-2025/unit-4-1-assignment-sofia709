5b544c10e7c4006aa0b7226cc6d84b0a5cf86319
sofia709 <vazquezsofi0519@gmail.com>
2024-12-13 21:47:11 +0000

    l

--- classwork.cpp
+++ classwork.cpp
@@ -3,15 +3,6 @@
 
 int main()
 {
-int num =5;
-num=10;
-
-std :: string stu_name = "John";
-std :: string number = "5";
-
-std :: << "My name is"<< stu_name << std :: endl;
-
-  
 
   //Exercise 1
   int x=25;
@@ -20,10 +11,26 @@ std :: << "My name is"<< stu_name << std :: endl;
   std ::cout << "x+y="<< x+y<< std::endl;
   std :: cout << "x-y=" << x-y << std :: endl;
   std:: cout << "x*y=" << x*y << std :: endl;
-  std :: cout << "x/y" << x/y << std :: endl;
-  std :: cout << "x%y" << x%y << std :: endl;
+  std :: cout << "x/y=" << x/y << std :: endl;
+  std :: cout << "x%y=" << x%y << std :: endl;
   
+  int z = 10;
+  std::cout << "x > z: " << (x > z) << std::endl;
+  std::cout << "y < x: " << (y < x) << std::endl;
 
+ //Exercise 2
+  bool isSunny = true;
+  bool hasUmbrella = false;
+
+  std::cout << "Exercise 2:" << std::endl;
+  std::cout << "isSunny && hasUmbrella: " << (isSunny && hasUmbrella) 
+  << std::endl;
+  std::cout << "isSunny || hasUmbrella: " << (isSunny || hasUmbrella) 
+  << std::endl;
+  std::cout << "!isSunny: " << (!isSunny) << std::endl;
+  std::cout << "!isSunny && !hasUmbrella: " << (!isSunny &&
+  !hasUmbrella) << std::endl;
+  std::cout << std::endl;
 
   return 0;
 }
